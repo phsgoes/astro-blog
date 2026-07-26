9e64c56ad6388f72e3980534bbbcd0c3509cbc5d
Paulo Henrique S. Goes <phsgoes@outlook.com>
2026-07-26 00:19:13 -0300

    fix: SITE_DOMAIN

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
@@ -8,9 +11,28 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: posts.map((post) => {
+      // Garante que a URL da imagem seja absoluta (ex: https://seusite.com)
+      const imageUrl = post.data.heroImage.src.startsWith('https') 
+        ? post.data.heroImage.src
+        : `${String(context.site).replace(/\/$/, '')}${post.data.heroImage.src}`;
+
+      return {
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/blog/${post.data.slug}/`,
+        // Injetamos a imagem em uma tag personalizada que o Make consegue ler perfeitamente
+        customData: `
+          <media:content url="${imageUrl}" medium="image"  type="image/jpeg" />
+        `,
+				content: sanitizeHtml(parser.render(post.body)),
+				...post.data,
+      };
+    }),
+    // Habilita o namespace de mídia no XML para o Make reconhecer a tag acima
+    xmlns: {
+      media: 'http://yahoo.com',
+    },
 	});
 }
