17efa02c8d2c28ede412fa3dea0ca084c0bba136
Paulo Henrique S. Goes <phsgoes@outlook.com>
2026-07-26 15:21:05 -0300

    fix: SITE_DOMAIN

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://phsgoes.github.io',
-	// base: '/astro-blog',
+	base: '/astro-blog',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 	fonts: [
