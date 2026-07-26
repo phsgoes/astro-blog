12217dfc23349f07c537bc943f1550f959450259
Paulo Henrique S. Goes <phsgoes@outlook.com>
2026-07-26 16:29:24 -0300

    fix: SITE_DOMAIN

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import { SITE_URL, SITE_BASE } from "../astro.config.mjs";
+
+export const SITE_PATH = String(`${SITE_URL}${SITE_BASE}`).slice(0, -1);
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'My first Astro Blog!';
