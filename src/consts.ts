5e3e190496f5889f25855b617389666917162382
Paulo Henrique S. Goes <phsgoes@outlook.com>
2026-07-26 16:36:16 -0300

    fix: SITE_DOMAIN

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 import { SITE_URL, SITE_BASE } from "../astro.config.mjs";
 
-export const SITE_PATH = String(`${SITE_URL}${SITE_BASE}`).slice(0, -1);
+export const SITE_PATH = String(`${SITE_URL}${SITE_BASE}`).replace(/\/+$/, '') + '/';
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'My first Astro Blog!';
